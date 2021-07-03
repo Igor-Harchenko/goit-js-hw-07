2a119b9362a5eae1065ed4a10263286bda1ba07c
Igor Harchenko <1305hariok@ukr.net>
2021-07-03 17:57:45 +0300

    Выполнены 7 заданий дз № 7

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const navEl = document.querySelector('#ingredients');
+const addItems = ingredients =>
+  ingredients.map((ingredient) => {
+    const navItemEl = document.createElement('li');
+    navItemEl.textContent = ingredient;
+    return navItemEl;
+  });
+  const listEl = addItems(ingredients);
+navEl.append(...listEl);
